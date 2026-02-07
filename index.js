0b54f928805d69459bb7c22ee31632f894b56cce
Rubén Katins Durán <katinsruben.98@gmail.com>
2026-02-08 00:56:56 +0100

    Instalar `React Navigation`

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import { PerfilView } from './assets/components/views/PerfilView';
+import { NavigationPerfil } from './assets/scripts/navigation/NavigationPerfil';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(PerfilView);
+registerRootComponent(NavigationPerfil);
