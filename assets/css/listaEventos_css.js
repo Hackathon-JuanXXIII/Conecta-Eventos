c31a266e55adb2f56b3be0d6a00b7f07d7e122bd
Rubén Katins Durán <katinsruben.98@gmail.com>
2026-02-09 23:12:56 +0100

    No debi importar bien al juntar las ramas

---
+++ assets/css/listaEventos_css.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const listaEventos_css = StyleSheet.create({
+    screen: {
+		alignItems: 'center',
+		paddingVertical: 15
+	},
+    lista: {
+        flex: 1
+    }
+})
